9dcc3f7992c3b6039cc800d2bb3cc83d96872f53
Siyuan Zhang <zsyk899@gmail.com>
2015-04-30 22:57:41 +1000

    Remove admin info

--- replication_test/test.sh
+++ replication_test/test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-curl -X PUT "http://siyuan:zsy91067@localhost:5984/t1"
-curl -X PUT "http://siyuan:zsy91067@localhost:5984/t2"
+curl -X PUT "http://localhost:5984/t1"
+curl -X PUT "http://localhost:5984/t2"
 
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "1", "name": "lets"}'
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "2", "name": "see"}'
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "4", "name": "if"}'
+curl -X POST "http://localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "1", "name": "lets"}'
+curl -X POST "http://localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "2", "name": "see"}'
+curl -X POST "http://localhost:5984/t1" --header "Content-Type:application/json" --data '{"_id": "4", "name": "if"}'
 
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "1", "name": "this"}'
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "2", "name": "thing"}'
-curl -X POST "http://siyuan:zsy91067@localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "3", "name": "works"}'
+curl -X POST "http://localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "1", "name": "this"}'
+curl -X POST "http://localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "2", "name": "thing"}'
+curl -X POST "http://localhost:5984/t2" --header "Content-Type:application/json" --data '{"_id": "3", "name": "works"}'
 #replicate t1 -> t2, 'this thing works if' on t2
