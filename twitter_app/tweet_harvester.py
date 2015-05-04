93b4f7951de7be3a7fc11b1739e24f037b1dbfc9
Siyuan Zhang <zsyk899@gmail.com>
2015-05-04 17:39:42 +1000

    Add python code for querying N most talked topics
    It can be used to create design view for count occurrence of each topic
    and sort the returned results in descending order.

--- twitter_app/tweet_harvester.py
+++ twitter_app/tweet_harvester.py
@@ -49,7 +49,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 #set up couchdb (local version)
 #put in your own db name and address
-db_name = 'twit3'
+db_name = 'twit'
 server_location = "http://localhost:5984/"
 couch = couchdb.Server(server_location)
 db = couch[db_name]
@@ -57,65 +57,64 @@ db = couch[db_name]
 
 
 class listener(StreamListener):
-	""" A listener handles tweets received from the stream.
-	This is a custom listener that store received tweets to FILE.
-	"""
-	def on_data(self, tweet_data):
-		try:
-			#converts to json format then saves in couchdb
-			tweets_json = json.loads(tweet_data)
-			doc_id = tweets_json["id_str"]
-			tweet_lang = tweets_json["lang"]
-			#gets data from meaningcloud service
-			meaningcloud_data = sendPost(tweets_json["text"], tweet_lang)
-			if meaningcloud_data is None:
-				#if invalid language, topic and sentiment is not added to document
-				doc = {"_id": doc_id, "tweet_data": tweets_json}
-			else: 
-				sentiment_topic = meaningcloud_data.read()
-				r = json.loads(sentiment_topic.decode())
+    # A listener handles tweets received from the stream.
+    # This is a custom listener that store received tweets to FILE.
+    def on_data(self, tweet_data):
+        try:
+            #converts to json format then saves in couchdb
+            tweets_json = json.loads(tweet_data)
+            doc_id = tweets_json["id_str"]
+            tweet_lang = tweets_json["lang"]
+            #gets data from meaningcloud service
+            meaningcloud_data = sendPost(tweets_json["text"], tweet_lang)
+            if meaningcloud_data is None:
+                #if invalid language, topic and sentiment is not added to document
+                doc = {"_id": doc_id, "tweet_data": tweets_json}
+            else:
+                sentiment_topic = meaningcloud_data.read()
+                r = json.loads(sentiment_topic.decode())
 
-				#handling of API call limit
-				counter = int(r["status"]["remaining_credits"])
-				if counter < 100:
-					print ("""There are less than 100 API calls left on the current account. 
-							Please insert a new key in meaningcloud_client.py""")
-				elif counter < 10:
-					print ("""There are less than 10 API calls left on the current account. 
-							Please insert a new key in meaningcloud_client.py""")
-					print ("Terminating...")
-					sys.exit(0)
+                #handling of API call limit
+                counter = int(r["status"]["remaining_credits"])
+                if counter < 100:
+                    print ("""There are less than 100 API calls left on the current account.
+                            Please insert a new key in meaningcloud_client.py""")
+                elif counter < 10:
+                    print ("""There are less than 10 API calls left on the current account.
+                            Please insert a new key in meaningcloud_client.py""")
+                    print ("Terminating...")
+                    sys.exit(0)
 
-				#id of the document is the tweet id
-				#meaningcloud data is added as attribute in the document
-				doc = {"_id": doc_id, "tweet_data": tweets_json, "meaningcloud": r}
+                #id of the document is the tweet id
+                #meaningcloud data is added as attribute in the document
+                doc = {"_id": doc_id, "tweet_data": tweets_json, "meaningcloud": r}
 
-			#saves the document to database
-			db.save(doc)
-			print('added: ' + doc_id)
-			return True
-		except BaseException as e:
-			print(e)
-			time.sleep(5)
-		except couchdb.http.ResourceConflict:
-			#handles duplicates
-			time.sleep(5)
+            #saves the document to database
+            db.save(doc)
+            print('added: ' + doc_id)
+            return True
+        except BaseException as e:
+            print(e)
+            time.sleep(5)
+        except couchdb.http.ResourceConflict:
+            #handles duplicates
+            time.sleep(5)
 
-	def on_error(self,status):
-		#returning False in on_data disconnects the stream
-		if status_code == 420:
-			print(status)
-			return False
+    def on_error(self,status):
+        #returning False in on_data disconnects the stream
+        if status_code == 420:
+            print(status)
+            return False
 			
 			
 def main():
-	print("Streaming started.... Ctrl+C to abort")
-	try:
-		twitterStream = Stream(auth,listener())
-		twitterStream.filter(locations = GEOBOX_BHAM)
-	except Exception as e:
-		print (e)
-		print("Error or execution finished. Program exiting... ")
-		twitterStream.disconnect()
+    print("Streaming started.... Ctrl+C to abort")
+    try:
+        twitterStream = Stream(auth,listener())
+        twitterStream.filter(locations = GEOBOX_BHAM)
+    except Exception as e:
+        print (e)
+        print("Error or execution finished. Program exiting... ")
+        twitterStream.disconnect()
 
 main()
