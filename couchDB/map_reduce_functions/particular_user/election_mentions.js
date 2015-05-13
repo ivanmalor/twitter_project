9456964bbe76747b29fbfe4759acd30a4d69518d
Siyuan Zhang <zsyk899@gmail.com>
2015-05-13 15:27:18 +1000

    Add code for election mentions and sentiment
    1. Add code for election mentions
    2. Add code for election sentiment
    3. Update code of user_tweet_time_duration and
    user_sentiment_time_duration
    Change the output to {[year, month, day, hour], count}.

---
+++ couchDB/map_reduce_functions/particular_user/election_mentions.js
@@ -0,0 +1,77 @@
+//Use _sum as reduce function.
+//group_level = 2 for the number of mentions on different period of each party
+//group_level = 1 for the number of mentions on each party
+//group_level = 0 for the total number of mentions of the general election
+
+/* Conservative Party */
+var cons_party = ['david','cameron','conservative', '#conservative', ];
+/* Labour Party */
+var lab_party = ['ed','miliband','labour', '#labour'];
+/* Liberal Democrat Party*/
+var lib_party = ['nick', 'clegg','liberal', '#liberal', '#libdems'];
+/* Scottish National Party*/
+var snp = ['nicola', 'sturgeon', 'snp', '#snp', '#votesnp'];
+/* UK Independence Party */
+var ukip = ['ukip', 'nigel', 'farage', '#ukip'];
+/* Greens */
+var green = ['green', 'greens', 'farage', '#green', '#greens', 'natalie', 'bennett'];
+
+/* pre-election date - May 7th, 2015 */
+// Any tweets before this date are classified as pre-election tweets
+pre_ele_date = new Date(2015, 4, 7)
+
+/* post-election date - May 8th, 2015 */
+// Any tweets after this date are classified as post-election tweets
+post_ele_date = new Date(2015, 4, 8)
+
+function(doc) {
+
+    if(doc.tweet_data.entities.hashtags){
+        doc.tweet_data.entities.hashtags.forEach(
+        function(hashtag){
+            topic = hashtag.text.toLowerCase();
+            if(topic == "ge2015" || topic == "ge15"){
+                tweet_words = doc.tweet_data.text.toLowerCase().split(" ");
+
+                tweet_words.forEach(
+                function(word){
+                    var party = getParty(word);
+                    if(party!=null){
+                        date_obj = new Date(Date.parse((doc.tweet_data.created_at)));
+                        var period = getPeriod(date_obj);
+                        emit([party, period], 1);
+                    }
+
+                });
+
+            }
+        });
+    }
+}
+
+function getParty(word){
+   if(cons_party.indexOf(word) > -1){
+       return "conservative";
+   }else if(lab_party.indexOf(word) > -1){
+       return "labour";
+   }else if(lib_party.indexOf(word) > -1){
+       return "liberal";
+   }else if(snp.indexOf(word) > -1){
+       return "snp";
+   }else{
+       return null;
+   }
+}
+
+function getPeriod(date){
+    if(date < pre_ele_date){
+        //pre-election
+        return "pre-election"
+    }else if(date > pre_ele_date){
+        //post-election
+        return "post-election"
+    }else{
+        //in-election
+        return "In-election"
+    }
+}
--- couchDB/map_reduce_functions/particular_user/user_sentimnet_time_duration.js
+++ couchDB/map_reduce_functions/particular_user/user_sentimnet_time_duration.js
@@ -1,17 +1,21 @@
 //Get the sentiment of a particular user on different time duration
 //Use _sum as reduce function
-//Use group_level = 2 to get the sentiment for different hours on different days
-//Use group_level = 1 to get the sentiment for different days
-//Use group_level = 0 to get the overall sentiment for this user
+//Use group_level = 4 to get the sentiment for different hours
+//Use group_level = 3 to get the sentiment for different dates
+//Use group_level = 2 to get the sentiment for different months
+//Use group_level = 1 to get the sentiment for different years
+//Use group_level = 0 to get the overall sentiment of tweets for this user
 
 function(doc) {
 //Search for tweets of Birmingham Airport
-    if(doc.tweet_data.user.id == 52184891){
+    if(doc.tweet_data.user.id == 52184891 && doc.meaningcloud.score){
         date_obj = new Date(Date.parse((doc.tweet_data.created_at)));
         hour = date_obj.getUTCHours().toString();
-        key = date_obj.toLocaleDateString();
-        if(doc.meaningcloud.score){
- 		    emit([key,hour], parseFloat(doc.meaningcloud.score));
- 	    }
+        year = date_obj.getUTCFullYear().toString();
+        //getUTCMonth() returns value from 0 to 11
+        month = (date_obj.getUTCMonth()+1).toString();
+        date = date_obj.getUTCDate().toString();
+        emit([year, month, date, hour], parseFloat(doc.meaningcloud.score));
+
     }
 }
